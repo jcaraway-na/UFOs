5b400ea9624fb656a34192ad28f5c29f463ef899
Jennings Caraway <82488956+jcaraway-na@users.noreply.github.com>
2022-08-21 13:01:04 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // import the data from data.js
-// const tableData = data;
+const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody")
@@ -63,3 +63,29 @@ function listLoop(userList) {
       );
     });
   }
+
+  // We'll need to add a second function to our code that will focus on filtering the table we just built.
+  function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  }
+  
+  // Attach an event to listen for the form button
+  d3.selectAll("#filter-btn").on("click", handleClick);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
